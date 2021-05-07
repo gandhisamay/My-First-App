efd9a00ed05c9f3f91d1dfade53c12567d76fd4d
gandhisamay <gandhisamay200@gmail.com>
2021-05-07 07:37:48 +0530

    All errors solved
    All the errors pertaining to the body widget are solved and now building upon the features

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,56 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
-class MyApp extends StatefulWidget
-{
+class MyApp extends StatefulWidget {
   @override
-  State<StatefulWidget> createState()
-  {
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
     return MyAppState();
   }
 }
 
-class  MyAppState extends State<MyApp> {
+class MyAppState extends State<MyApp> {
   var index = 0;
-  var questions = ['What is your gender?',
-                   'How is your health ?'];
+  var questions = ['What is your gender ?', 'How is your health ?'];
+  var answers = [
+    ['Male', 'Female', 'Transgender'],
+    ['Excellent', 'Good', 'Bad']
+  ];
 
-  var options = [['Male','Female','TransGender'],
-                 ['Excellent','Good','Bad']];
-  void answerQuestion()
-  {
+  void answerQuestion(){
     setState((){
-      index = index + 1;
-      });
+      index += 1;
+    });
   }
 
   @override
-  Widget build(BuildContext context)
-  {
-    return MaterialApp(home : Scaffold(
-      appBar: AppBar(
-      title : Text('My First App'),),
-
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home : Scaffold(
+        appBar: AppBar(
+          title : Text('Quiz App'),
+       ),
       body : Column(
-        children:
-        Text(questions[index]),
-        // ignore: deprecated_member_use
-        RaiseButton(
-          child:Text(answers[index][0]),
-          onPress : answerQuestion),
-        // ignore: deprecated_member_use
-        RaisedButton(
-          child:Text(answers[index][1]),
-          OnPressed : answerQuestion),
-        // ignore: deprecated_member_use
-        RaisedButton(
-          child:Text(answers[index][2])),
-          OnPressed : answerQuestion)));
+        children: [
+          Text(questions[index]),
+          // ignore: deprecated_member_use
+          RaisedButton(
+            child : Text(answers[index][0]),
+            onPressed: answerQuestion),
+          // ignore: deprecated_member_use
+          RaisedButton(
+            child : Text(answers[index][1]),
+            onPressed: answerQuestion),
+          // ignore: deprecated_member_use
+          RaisedButton(
+            child : Text(answers[index][2]),
+            onPressed: answerQuestion),  
+        ],)
+     )
+  );
+
   }
 }
