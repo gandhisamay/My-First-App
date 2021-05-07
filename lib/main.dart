16b96a87d6d973e40ec4f8c0cbfa86a818c8c474
gandhisamay <gandhisamay200@gmail.com>
2021-05-07 08:24:27 +0530

    Made changes so that the question.dart file can be used

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -20,37 +22,32 @@ class MyAppState extends State<MyApp> {
     ['Excellent', 'Good', 'Bad']
   ];
 
-  void answerQuestion(){
-    setState((){
+  void answerQuestion() {
+    setState(() {
       index += 1;
     });
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      home : Scaffold(
-        appBar: AppBar(
-          title : Text('Quiz App'),
-       ),
-      body : Column(
-        children: [
-          Text(questions[index]),
-          // ignore: deprecated_member_use
-          RaisedButton(
-            child : Text(answers[index][0]),
-            onPressed: answerQuestion),
-          // ignore: deprecated_member_use
-          RaisedButton(
-            child : Text(answers[index][1]),
-            onPressed: answerQuestion),
-          // ignore: deprecated_member_use
-          RaisedButton(
-            child : Text(answers[index][2]),
-            onPressed: answerQuestion),  
-        ],)
-     )
-  );
-
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Quiz App'),
+            ),
+            body: Column(
+              children: [
+                Question(questions[index]),
+                // ignore: deprecated_member_use
+                RaisedButton(
+                    child: Text(answers[index][0]), onPressed: answerQuestion),
+                // ignore: deprecated_member_use
+                RaisedButton(
+                    child: Text(answers[index][1]), onPressed: answerQuestion),
+                // ignore: deprecated_member_use
+                RaisedButton(
+                    child: Text(answers[index][2]), onPressed: answerQuestion),
+              ],
+            )));
   }
 }
